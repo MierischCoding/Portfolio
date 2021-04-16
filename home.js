7907e8631f41ba0e7b6b4714787c6efd9026bc61
Jonathan Mierisch <mierischjonathan@yahoo.com>
2021-04-15 19:13:11 -0600

    added JS cannot get it to link to my script.

---
+++ home.js
@@ -0,0 +1,4 @@
+var x;
+x = prompt("Welcome! Please enter your name so we can get to know each other.")
+alert("Welcome "+x+" thank you for visiting my page!")
+document.getElementById("x").innerHTML = "Hello " + x + "," + " please use the link below to explore my Portfolio";
