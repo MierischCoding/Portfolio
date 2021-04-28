1fb7b3be3b95c4a9f0f7e5a6acbd43f437d0588e
Jonathan Mierisch <mierischjonathan@yahoo.com>
2021-04-28 11:21:48 -0600

    updated with week 2 assignment.

--- home.js
+++ home.js
@@ -1,11 +1,13 @@
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
-
 var name = prompt("Welcome! Please enter your name so we can get to know each other.");
 
-if (hourNow > 18) {greeting = "Good evening "+ name + " thank you for visiting my page!";}
-else if (hourNow > 12) {greeting = "Good afternoon "+ name + " thank you for visiting my page!";}
-else {greeting ="Good morning "+ name + " thank you for visiting my page!";}
+if (hourNow > 18) 
+{greeting = "Good evening "+ name + " thank you for visiting my page!";}
+    else if (hourNow > 12) 
+{greeting = "Good afternoon "+ name + " thank you for visiting my page!";}
+    else 
+{greeting ="Good morning "+ name + " thank you for visiting my page!";}
 
 alert(greeting);
