7a1eb6433a6efa71b4a1ae1d14452e8b6f9c5fba
Jonathan Mierisch <mierischjonathan@yahoo.com>
2021-06-07 13:20:44 -0600

    hi

--- home.js
+++ home.js
@@ -4,12 +4,16 @@ var greeting;
 
 function myFunction(){
 var name = prompt("Welcome! Please enter your name so we can get to know each other."); 
-    if (hourNow > 18) 
-    {greeting = "Good evening "+ name + " thank you for visiting my page!";}
-    else if (hourNow > 12) 
-    {greeting = "Good afternoon "+ name + " thank you for visiting my page!";}
-    else 
-    {greeting ="Good morning "+ name + " thank you for visiting my page!";}
+
+    if (hourNow > 18)  {
+        greeting = "Good evening "+ name + " thank you for visiting my page!";
+    }
+    else if (hourNow > 12)  {
+        greeting = "Good afternoon "+ name + " thank you for visiting my page!";
+    }
+    else  {
+        greeting ="Good morning "+ name + " thank you for visiting my page!";
+    }
 
 alert(greeting);
 }
